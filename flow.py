4f64ba06d4ad8f5e5f906124d336a4c5851fc006
IbrahimSayyad2001 <159575444+IbrahimSayyad2001@users.noreply.github.com>
2024-02-11 23:00:02 +0530

    flow.py

---
+++ flow.py
@@ -0,0 +1,12 @@
+# Write a program for students to declare their result 
+
+marks= int( input('Enter your marks'))
+if marks>= 70 :
+    print('you got first class ')
+elif marks>= 50:
+        print('you got second class')
+elif marks>= 40:
+            print('you just passed')
+else :
+                print ('you have failed ')
+------------------------------------------
