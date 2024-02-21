b22b0d9af6fbcffe7804df0c3be9848c99b7020e
IbrahimSayyad2001 <159575444+IbrahimSayyad2001@users.noreply.github.com>
2024-02-21 03:39:04 -0800

    Create oop.py

---
+++ oop.py
@@ -0,0 +1,30 @@
+""""""
+"""
+advance  python 
+class sample:
+    def __init__(self, name,age,salary):
+        print(name,age,salary)
+sample('amit',44,9800)
+_________________________
+class sample:
+    def __init__(self, name,age,salary):
+        print(name,age,salary)
+sample(age=44,salary=9800,name='adil')
+____________________________
+class bankaccount:
+    def __init__(self,account_number,balance,IFCS_code ):
+        print(account_number,balance,IFCS_code)
+bankaccount(144,4300,'ib4002')
+____________________________
+class dmartactivities:
+    def __init__(self,items,quantity,bill):
+        print(items,quantity,bill)
+dmartactivities([1,2,3,4,5],[1,2,3,4,5],12000)
+
+
+
+"""
+
+
+
+
